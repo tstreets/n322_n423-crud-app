8f88d681d267a6226ef58a00e77e0fe4dd736b16
Ty Streets <=>
2023-04-16 16:08:06 -0400

    Able to login / logout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import '@/styles/globals.css';
+import useFirebase from '../useHooks/useFirebase';
+import Link from 'next/link';
 
 export default function App({ Component, pageProps }) {
+	const firebase = useFirebase();
 	return (
 		<>
+			<nav>
+				<ul>
+					<li>
+						<Link href='/'>Home</Link>
+					</li>
+					<li>
+						{firebase.currentUser.email ? (
+							<button onClick={firebase.logoutUser}>Logout</button>
+						) : (
+							<button onClick={firebase.loginUser}>Login</button>
+						)}
+					</li>
+				</ul>
+			</nav>
 			<Component {...pageProps} />
 		</>
 	);
