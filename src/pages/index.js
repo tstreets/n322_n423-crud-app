0b6b345cefb0daf60f66300168589ee639e512d0
Ty Streets <=>
2023-04-16 20:41:51 -0400

    Pulling in data

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,22 @@ import useFirebase from '../useHooks/useFirebase';
 
 export default function HomePage() {
 	const firebase = useFirebase();
+	const [booksList, setBooksList] = React.useState([]);
+
+	const booksListComponents = booksList.map(book => {
+		return <li key={book.id}>{book.name}</li>;
+	});
+
+	async function pullBooksFromDb() {
+		const books = await firebase.getBooks();
+		setBooksList(books);
+	}
+
 	return (
 		<>
 			<h1>My Name: {firebase.currentUser.displayName || '--'}</h1>
+			<button onClick={pullBooksFromDb}>Get Books</button>
+			<ul>{booksListComponents}</ul>
 		</>
 	);
 }
--- src/useHooks/useFirebase.js
+++ src/useHooks/useFirebase.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -13,6 +14,7 @@ const firebaseConfig = {
 const app = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth(app);
 const googleProvider = new firebase.auth.GoogleAuthProvider();
+const db = firebase.firestore(app);
 
 export default function useFirebase() {
 	const initialUser = { email: '', displayName: '' };
@@ -38,6 +40,18 @@ export default function useFirebase() {
 		async logoutUser() {
 			await auth.signOut();
 			return {};
+		},
+		async getBooks() {
+			const booksSnapshot = await db.collection('books').get();
+			const booksList = [];
+			for (let book of booksSnapshot.docs) {
+				const bookData = book.data();
+				booksList.push({
+					...bookData,
+					id: book.id,
+				});
+			}
+			return booksList;
 		},
 	};
 }
